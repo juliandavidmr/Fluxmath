1da941f46f084a330d55253550b9a0395db1c6c0
juliandavidmr <anlijudavid@hotmail.com>
2017-03-06 01:29:51 -0500

    formularios de creacion, y captura de parametros para markov

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.publicacion_list)
+    url(r'^$', views.publicacion_list),
+    url(r'^publicacion/(?P<pk>[0-9]+)/$', views.publicacion_detalle),
+    url(r'^publicacion/crear/$', views.publicacion_crear, name='publicacion_crear'),
+    url(r'^markov/steps/$', views.markov_steps)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Publicacion
+from .forms import PublicacionForm
+
+from .markov import *
 
 # Create your views here.
 
+
 def publicacion_list(request):
-    return render(request, 'blog/publicacion_list.html', {})
+    publicaciones = Publicacion.objects.order_by('publicado_fecha')
+    return render(request, 'blog/publicacion_list.html', {'publicaciones': publicaciones})
+
+
+def publicacion_detalle(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/publicacion_detalle.html', {'publicacion': publicacion})
+
+
+def publicacion_crear(request):
+    if request.method == "POST":
+        form = PublicacionForm(request.POST)
+    else:
+        form = PublicacionForm()
+    return render(request, 'blog/publicacion_crear.html', {'form': form})
+
+
+def markov_steps(request):
+    if request.method == "POST":
+        inp = request.POST.get('matrix', '')  # => [137]
+        ste = request.POST.get('steps', '')  # => [137]
+        matrix_input = stringToMatrix(input_str=inp)
+        result = nextSteps(matrix_input, steps=int(ste))
+        return render(request, 'blog/markov_steps.html', {'result': result})
+    else:
+        return render(request, 'blog/markov_steps.html', {})
