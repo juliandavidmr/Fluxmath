972ad432a3640a069c87e82a24123d02a1bafb8f
juliandavidmr <anlijudavid@hotmail.com>
2017-03-09 23:18:09 -0500

    update config

--- FluxMath/settings.py
+++ FluxMath/settings.py
@@ -110,7 +110,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
